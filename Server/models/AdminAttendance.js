d5b183ddfe9c3cac09f4fd41931a2a9e39fa113f
Md. Sajedul Islam <sajedulislms@gmail.com>
2023-05-29 22:45:08 +0600

    Profile StudentAttendance and Admin attendance modal added

---
+++ Server/models/AdminAttendance.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require("mongoose");
+
+const adminAttendanceSchema = new Schema({
+    timeLimit: {
+        type: Number,
+        required: true,
+        max: 30,
+        min: 1,
+        default: 5,
+    },
+    status: {
+        type: String,
+        required: true,
+        enum:['RUNNING','COMPLETED'],
+        default: 'RUNNING'
+    }
+},{timestamps: true})
+
+const AdminAttendance = model('AdminAttendacne',adminAttendanceSchema)
+module.exports = AdminAttendance
---
+++ Server/models/Profile.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require("mongoose");
+
+const profileSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    phone: String,
+    avatar: String,
+    user: {
+        type: Schema.Types.ObjectId,
+        ref:'User'
+    }
+})
+
+const Profile = model('Profile',profileSchema)
+module.exports = Profile
